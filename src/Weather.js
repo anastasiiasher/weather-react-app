8907f02f8292def4137104eb49157906e710956c
anastasiiasher <nastyasher@me.com>
2021-09-26 16:31:28 +0200

    Weather app bulit in React

---
+++ src/Weather.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+
+function Weather() {
+  return (
+    <div className="App">
+      <h1> Weather app</h1>
+    </div>
+  );
+}
+
+export default Weather;
